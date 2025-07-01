6cd4ebeb8da01a47457dbb03bbbd2c93cdabda80
Vicove74 <vicove@gmail.com>
2025-07-01 15:02:02 +0300

    Update mcp_wp_server.py

--- mcp_wp_server.py
+++ mcp_wp_server.py
@@ -306,3 +306,5 @@ def list_pages():
 if __name__ == '__main__':
     # Локално стартиране за тестване
     app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
